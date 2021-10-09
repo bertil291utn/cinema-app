6ea7991b090a0212ba75fbf5d987e2e19b994ca8
Bertil T <bertilt4@gmail.com>
2021-10-08 22:29:29 -0500

    :sparkles: add filter by cities

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,20 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
-  const cities = ['Tumbaco', 'Los Chillos', 'Cayambe', 'El Coca'];
-  let movies = [
-    {
-      id: 1,
-      name: 'Coda',
-      poster: 'https://www.cineplex.com.ec/customers/fotos/poster_coda.jpg',
-      new: true,
-    },
-    {
-      id: 2,
-      name: 'Huevitos',
-      poster: 'https://www.cineplex.com.ec/customers/fotos/poster_huevitos.jpg',
-      new: false,
-    },
-    {
-      id: 3,
-      name: 'Casa oscura',
-      poster:
-        'https://www.cineplex.com.ec/customers/fotos/poster_lacasaoscura2.jpg',
-      new: false,
-    },
-    {
-      id: 4,
-      name: 'James Bond 007',
-      poster: 'https://www.cineplex.com.ec/customers/fotos/poster_007.jpg',
-      new: true,
-    },
-    {
-      id: 4,
-      name: 'Venom',
-      poster: 'https://www.cineplex.com.ec/customers/fotos/poster_venom3.jpg',
-      new: true,
-    },
-    ,
-  ];
+  const [_movies, setMovies] = useState(sortMovies(movies, 'new'));
+
+  const selectByCity = (cityId) => () => {
+    setMovies(
+      sortMovies(
+        movies.filter((m) => m.cityId.includes(cityId)),
+        'new'
+      )
+    );
+  };
+
   //sort by new estrenos
-  movies = movies.sort((x, y) => (x.new === y.new ? 0 : x.new ? -1 : 1));
   return (
     <div>
       <Head>
@@ -52,24 +28,22 @@ export default function Home() {
           {cities.map((city, i) => (
             <button
               key={`btn-city-${i}`}
+              onClick={selectByCity(city.id)}
               className='rounded-full border border-black px-3 py-1 mr-3 mb-3 focus:bg-black focus:text-white'
             >
-              {city}
+              {city.name}
             </button>
           ))}
         </div>
         <div className='my-5 text-center grid grid-cols-2 gap-3'>
-          {movies.map((movie, i) => (
-            <div
-              key={`poster-${i}`}
-              className="h-96 w-full relative "
-            >
+          {_movies.map((movie, i) => (
+            <div key={`poster-${i}`} className='h-96 w-full relative '>
               <Image
                 src={movie.poster}
                 alt={`poster-${i}`}
-                layout="fill"
-                objectFit="cover" 
-                className="rounded-lg"
+                layout='fill'
+                objectFit='cover'
+                className='rounded-lg'
               />
               {movie.new && (
                 <span className='absolute left-1 top-1 inline-block rounded-full text-white bg-red-700 px-2 py-1 text-xs font-bold mr-3'>
@@ -83,3 +57,52 @@ export default function Home() {
     </div>
   );
 }
+
+export const cities = [
+  { id: 1, name: 'Tumbaco' },
+  { id: 2, name: 'Los Chillos' },
+  { id: 3, name: 'Cayambe' },
+  { id: 4, name: 'El Coca' },
+];
+export let movies = [
+  {
+    id: 1,
+    name: 'Coda',
+    poster: 'https://www.cineplex.com.ec/customers/fotos/poster_coda.jpg',
+    new: true,
+    cityId: [1, 2, 3, 4],
+  },
+  {
+    id: 2,
+    name: 'Huevitos',
+    poster: 'https://www.cineplex.com.ec/customers/fotos/poster_huevitos.jpg',
+    new: false,
+    cityId: [1, 2, 3, 4],
+  },
+  {
+    id: 3,
+    name: 'Casa oscura',
+    poster:
+      'https://www.cineplex.com.ec/customers/fotos/poster_lacasaoscura2.jpg',
+    new: false,
+    cityId: [1, 3, 4],
+  },
+  {
+    id: 4,
+    name: 'James Bond 007',
+    poster: 'https://www.cineplex.com.ec/customers/fotos/poster_007.jpg',
+    new: true,
+    cityId: [1, 2, 3, 4],
+  },
+  {
+    id: 5,
+    name: 'Venom',
+    poster: 'https://www.cineplex.com.ec/customers/fotos/poster_venom3.jpg',
+    new: true,
+    cityId: [1, 2],
+  },
+  ,
+];
+
+export const sortMovies = (movies, field) =>
+  movies.sort((x, y) => (x[field] === y[field] ? 0 : x[field] ? -1 : 1));
