d967e15c8c9666cc0fdc85bc61ce986b15ea9ebc
Bertil T <bertilt4@gmail.com>
2021-10-17 22:12:57 -0500

    :zap: display left tabs as dinamycal dta from get props

--- pages/film/[fid].js
+++ pages/film/[fid].js
@@ -3,11 +3,18 @@ import { getPlaiceholder } from 'plaiceholder';
 import Layout from '../../components/Layout';
 import { db } from '../../initFirebase';
 import Link from 'next/link';
+import { useState,useEffect } from 'react';
+
 
 import { IoChevronBackOutline, IoPlay } from 'react-icons/io5';
 import Image from 'next/image';
 
-const FilmDetail = ({ film }) => {
+const FilmDetail = ({ film,lefTabs }) => {
+  
+  const [_leftTabs, setLeftTabs] = useState(lefTabs);
+
+  const updateTabs = (tabId) => () => {};
+
   return (
     <Layout subDir>
       {film ? (
@@ -40,12 +47,22 @@ const FilmDetail = ({ film }) => {
           </div>
 
           <div className='flex flex-row pl-6'>
-            <ul className='flex flex-col justify-between items-start h-52 text-lg'>
-              <li className='text-rotate-90-rl'>Info</li>
-              <li className='text-rotate-90-rl'>Horarios</li>
-              <li className='text-rotate-90-rl'>Cast</li>
+            <ul className='flex flex-col justify-between items-start h-64 text-lg'>
+              {_leftTabs.map((t) => (
+                <li
+                  key={`tab-${t.id}`}
+                  className={`text-rotate-90-rl ${
+                    t.active ? 'font-bold' : 'text-gray-400'
+                  }`}
+                  onClick={updateTabs(t.id)}
+                >
+                  {t.name}
+                </li>
+              ))}
             </ul>
-            <div className='mt-5 ml-8'>this is movie info</div>
+            <div className='mt-5 ml-8'>
+              <Horarios/>
+            </div>
           </div>
         </>
       ) : (
@@ -86,9 +103,29 @@ export async function getStaticProps({ params }) {
 
   console.log('film', film);
 
+  const lefTabs = [
+    { id: 1, name: 'Info', active: true},
+    { id: 2, name: 'Horarios', active: false},
+    { id: 3, name: 'Cast', active: false},
+  ];
+
   return {
     props: {
       film,
+      lefTabs
     },
   };
 }
+
+
+export const Info = () => {
+  return <div>this is info</div>;
+};
+
+export const Horarios = () => {
+  return <div>this is horarios</div>;
+};
+
+export const Cast = () => {
+  return <div>this is cast</div>;
+};
