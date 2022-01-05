66ecdf8ed0f902692901778aba9dabb2e6d27eae
Bertil T <bertilt4@gmail.com>
2022-01-04 22:41:47 -0500

    ✨Hotjar tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -49,14 +49,14 @@ function MyApp({ Component, pageProps }) {
           property='og:image'
           content='https://res.cloudinary.com/btandayamo/image/upload/v1641352715/volkfire-app/cineplex-og-image_jhpb98.png'
         />
-         {/* Global Site Tag (gtag.js) - Google Analytics */}
-         <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -64,8 +64,24 @@ function MyApp({ Component, pageProps }) {
               page_path: window.location.pathname,
             });
           `,
-            }}
-          />
+          }}
+        />
+
+        {/* <!-- Hotjar Tracking Code for https://cinema-app-theta.vercel.app --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </CinemaProvider>
